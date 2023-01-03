5b211a2bc2e03fb497d66be9ca2e456d5e3a6f8a
moremans <51314534+moremans@users.noreply.github.com>
2023-01-03 15:46:46 +0100

    lay-out and word changes

--- 30seconds-adapation/test.js
+++ 30seconds-adapation/test.js
@@ -1,16 +1,39 @@
-const {readFileSync, promises: fsPromises} = require('fs');
+// const {readFileSync, promises: fsPromises} = require('fs');
 
-function syncReadFile(filename) {
- // const contents = readFileSync(filename, 'utf-8');
- const buffer = readFileSync(filename);
- const contents = buffer.toString();
- console.log(contents)
-  const arr = contents.split(/\r?\n/);
+// function syncReadFile(filename) {
+//  // const contents = readFileSync(filename, 'utf-8');
+//  const buffer = readFileSync(filename);
+//  const contents = buffer.toString();
+//  console.log(contents)
+//   const arr = contents.split(/\r?\n/);
 
-  console.log(arr); 
+//   console.log(arr); 
 
-  return arr;
-}
+//   return arr;
+// }
 
-const words = syncReadFile('./words.txt');
+// const words = syncReadFile('./words.txt');
+// console.log(words)
+
+//---------------------------------------
+// get filesystem module
+function readtxtfile(filename) {
+    const fs = require("fs");
+
+    // using the readFileSync() function
+    // and passing the path to the file
+    const buffer = fs.readFileSync(filename);
+
+    // use the toString() method to convert
+    // Buffer into String and split on lines 
+    const fileContent = buffer.toString();
+    const arr = fileContent.split(/\r?\n/)
+
+    console.log(fileContent);
+    console.log(arr);
+
+    return arr;
+} 
+
+words = readtxtfile('words.txt')
 console.log(words)
--- 30seconds-adapation/wordscripts.js
+++ 30seconds-adapation/wordscripts.js
@@ -13,23 +13,32 @@ function sleep(time) {
     return new Promise((resolve) => setTimeout(resolve,time));
 }
 
-// // read words from file
-// const {readFileSync, promises: fsPromises} = require('fs');
+// // get filesystem module
+// function readtxtfile(filename) {
+//     const fs = require("fs");
 
-// function syncReadFile(filename) {
-//   const contents = readFileSync(filename, 'utf-8');
+//     // using the readFileSync() function
+//     // and passing the path to the file
+//     const buffer = fs.readFileSync(filename);
 
-//   const arr = contents.split(/\r?\n/);
+//     // use the toString() method to convert
+//     // Buffer into String and split on lines 
+//     const fileContent = buffer.toString();
+//     const arr = fileContent.split(/\r?\n/)
 
-//  //  console.log(arr); // to debug
+//     console.log(fileContent);
+//     console.log(arr);
 
-//   return arr;
-// }
-// //const words = syncReadFile('./words.txt');
+//     return arr;
+// } 
+
+// const words = readtxtfile('words.txt')
 
 
 // randomize the words 
-const words = ["January", "February", "March", "April", "May", "June", "July"]; // to debug
+//const words = ['een ', 'twee', 'drie', 'vier', 'vijf'];  // to debug
+
+const words = ['spanning', 'stroomsterkte ', 'vermogen', 'energiegebruik', 'Watt', 'Volt', 'Ampère', 'V', 'A', 'P ', 'U ', 'I ', 'E ', 't ', 'kWh', 'W ', 'KiloWattUur', 'geleider', 'isolator', 'elektriciteit', 'elektrische energie ', 'materiaal dat geleidt ', 'materiaal dat isoleert ', 'serieschakeling ', 'parallelschakeling', 'lamp', 'schakelaar', 'batterij', 'spanningsbron', 'bliksem', 'dynamo', 'kring ', 'open ', 'energiebron', 'zonnecel', 'kernenergie', 'windenergie', 'oplaadpaal', 'stopcontact', 'eenheid', 'natuurkunde', 'Youtube', 'Disney', 'Segbroek College', 'Tesla auto', 'scooter', '2M3', 'TikTok', 'Mickey Mouse', 'McDonalds ', 'Den Haag', 'Adidas', 'Nike', 'Whatsapp ', 'Nederland '];
 
 function shuffle(words) {
     const random = Math.floor(Math.random() * words.length);
@@ -53,92 +62,92 @@ function flipWord() {
     }
     if (word_num == 3) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "haha";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 4) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "haha";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 5) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "stroomsterkte";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 6) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "vermogen";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 7) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "elektriciteit";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 8) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "geleider";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 9) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "isolator";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 10) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "serieschakeling";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 11) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "parallelschakeling";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 12) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "lamp";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 13) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "schakelaar";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 14) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "batterij";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 15) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "spanningsbron";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 16) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "energiegebruik";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 17) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "2M3";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 18) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "TikTok";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 19) {
         document.getElementById("word2").textContent = " ";
-        document.getElementById("word1").textContent = "Mickey Mouse";
+        document.getElementById("word1").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 20) {
         document.getElementById("word1").textContent = " ";
-        document.getElementById("word2").textContent = "bliksem";
+        document.getElementById("word2").textContent = shuffle(words);
         CardIsClicked = false;
     }
     if (word_num == 21) {
