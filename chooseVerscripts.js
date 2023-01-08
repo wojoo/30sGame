b74c7fcef07e3ab12a179f0f986e144ff2f274d7
moremans <51314534+moremans@users.noreply.github.com>
2023-01-08 15:00:22 +0100

    timer weg

--- chooseVerscripts.js
+++ chooseVerscripts.js
@@ -1,51 +1,54 @@
 const button = document.querySelectorAll('.but');
 
 function time() {
-    var element = document.getElementById("intro-words");
-    element.classList.toggle("hide");
-    var element = document.getElementById("intro-time");
-    element.classList.toggle("hide");
-    button.forEach(but => but.style.visibility = "hidden");
+    window.location = "card_word1.html";
+    // var element = document.getElementById("intro-words");
+    // element.classList.toggle("hide");
+    // var element = document.getElementById("intro-time");
+    // element.classList.toggle("hide");
+    // button.forEach(but => but.style.visibility = "hidden");
 
-    var element1 = document.getElementById("go");
-        element1.classList.toggle("show");
+    // var element1 = document.getElementById("go");
+    //     element1.classList.toggle("show");
 
-    var seconds = document.getElementById("go").textContent;
-    var count = setInterval(function() {
-        seconds--;
-        if (seconds > 0) {
-            document.getElementById("go").textContent = seconds;
-        } else if (seconds == 0) {
-            document.getElementById("go").textContent = "Go!";
-        } else {
-            clearInterval(count);
-            document.getElementById("go").textContent = "";
-            window.location = "card_word1.html";
-        }
-    }, 1000);
+    // var seconds = document.getElementById("go").textContent;
+    // var count = setInterval(function() {
+    //     seconds--;
+    //     // if (seconds > 0) {
+    //     //     document.getElementById("go").textContent = seconds;
+    //     // } else if (seconds == 0) {
+    //     //     document.getElementById("go").textContent = "Go!";
+    //     // } else {
+    //         clearInterval(count);
+    //         document.getElementById("go").textContent = "";
+    //         window.location = "card_word1.html";
+    //     //}
+    // }, 1000);
 }
 
 function word() {
-    var element = document.getElementById("intro-words");
-    element.classList.toggle("hide");
-    var element = document.getElementById("intro-time");
-    element.classList.toggle("hide");
-    button.forEach(but => but.style.visibility = "hidden");
+    // var element = document.getElementById("intro-words");
+    // element.classList.toggle("hide");
+    // var element = document.getElementById("intro-time");
+    // element.classList.toggle("hide");
+    // button.forEach(but => but.style.visibility = "hidden");
 
-    var element1 = document.getElementById("go");
-        element1.classList.toggle("show");
+    // var element1 = document.getElementById("go");
+    //     element1.classList.toggle("show");
+    
+        window.location = "card_word2.html";
 
-    var seconds = document.getElementById("go").textContent;
-    var count = setInterval(function() {
-        seconds--;
-        if (seconds > 0) {
-            document.getElementById("go").textContent = seconds;
-        } else if (seconds == 0) {
-            document.getElementById("go").textContent = "Go!";
-        } else {
-            clearInterval(count);
-            document.getElementById("go").textContent = "";
-            window.location = "card_word2.html";
-        }
-    }, 1000);
+    // var seconds = document.getElementById("go").textContent;
+    // var count = setInterval(function() {
+    //     seconds--;
+    //     // if (seconds > 0) {
+    //     //     document.getElementById("go").textContent = seconds;
+    //     // } else if (seconds == 0) {
+    //     //     document.getElementById("go").textContent = "Go!";
+    //     // } else {
+    //         clearInterval(count);
+    //         document.getElementById("go").textContent = "";
+    //         window.location = "card_word2.html";
+    //    // }
+    // }, 1000);
 }
